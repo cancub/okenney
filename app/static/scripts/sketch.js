881c65aa0ca6ee277f52751a2e0bd4f25a9335d3
Al <cancub@live.com>
2019-08-12 13:08:58 -0700

    Add sketch with PMR background

--- app/static/scripts/sketch.js
+++ app/static/scripts/sketch.js
@@ -1,30 +1,26 @@
 $(function() {
-	var sketchpadWidth = $( window ).width();
-	var sketchpadHeight = $( window ).height() - $('#sketch-header').height();
-	var sketchpad = Raphael.sketchpad("editor", {
-        width: sketchpadWidth,
-        height: sketchpadHeight,
-        editing: true
-    });
-
     var filename = 'pmr-carte.jpg';
+    var sketchpadWidth, sketchpadHeight;
 
     $.ajax({
         url: "/api/image/" + filename,
         success: function(resp) {
-            console.log(resp);
+            sketchpadWidth = resp.width;
+            sketchpadHeight = resp.height;
+            var sketchpad = Raphael.sketchpad("editor", {
+                width: sketchpadWidth,
+                height: sketchpadHeight,
+                editing: true
+            });
+            sketchpad.paper().image('/images/' + filename, 0, 0, sketchpadWidth, sketchpadHeight)
+        
+            $('#editor image').on('dragstart', false);
+        
+            // When the sketchpad changes, update the input field.
+            sketchpad.change(function() {
+                console.log(sketchpad.json());
+            });
         },
     });
 
-    var carte = $('#carte');
-    sketchpad.paper().image(carte);
-    $('#editor image').on('dragstart', false);
-
-    var filename = 'pmr-carte.jpg';
-
-
-    // When the sketchpad changes, update the input field.
-    sketchpad.change(function() {
-        console.log(sketchpad.json());
-    });
 });
