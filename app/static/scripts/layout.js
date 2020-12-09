99f1f9859bf27d0d14650f51458da289a0a5de59
AOK <alf@okenney.ca>
2020-12-09 19:46:18 +0000

    Get rid of apology dialog

--- app/static/scripts/layout.js
+++ app/static/scripts/layout.js
@@ -177,17 +177,6 @@ $( function () {
     }
     $(window).resize(calculerVariables);
 
-    $('#Dialog').dialog({
-        autoOpen: true,
-        modal: true,
-        minWidth: 400,
-        buttons: {
-            OK: function() {
-                $( this ).dialog( "close" );
-            }
-        }
-    });
-
     /* Cacher En-tete avec clique sur Corps */
     $(document).mouseup(function(e) {
         $.each(['Contenu', 'ColonneGauche','footer'], function (i, element) {
