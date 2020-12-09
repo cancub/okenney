7fc948471ec75fd469f135880f36a80f7f507a46
AOK <alf@okenney.ca>
2020-12-09 19:47:12 +0000

    Get rid off all that is cruft for the moment
    Focus on just loading articles

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template
 from flask_restful import Resource, Api
 
-# Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
@@ -20,26 +19,6 @@ db = SQLAlchemy(app)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
-# Import a module / component using its blueprint handler variable (mod_auth)
-from app.mod_auth.controllers import mod_auth as auth_module
-from app.mod_sketch.controllers import mod_sketch as sketch_module
-
-# API stuff
-from app.rest_api.sketch import ImageMeta 
-
-# Register blueprint(s)
-app.register_blueprint(auth_module)
-app.register_blueprint(sketch_module)
-
-api.add_resource(ImageMeta, '/api/image', '/api/image/<string:filename>')
-
-# app.register_blueprint(xyz_module)
-# ..
-
-# Build the database:
-# This will create the database file using SQLAlchemy
-db.create_all()
 
 @app.route('/')
 def index():
