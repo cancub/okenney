42bed38abc772c1114dc0a97c7465d7a20833df9
AOK <alf@okenney.ca>
2021-10-16 00:25:55 -0400

    [meta] add script and framework for adding and registering new articles
    This commit also uses the new metadata management to more efficiently load
    articles on the homepage.

--- app/mod_articles/__init__.py
+++ app/mod_articles/__init__.py
@@ -2,22 +2,17 @@ import os.path
 
 from app.mod_articles.models import Article as _Article
 
+IMAGES_PROJECT_PATH = 'static/images/articles'
+ARTICLES_PROJECT_PATH = 'templates/articles'
+ROOT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+)
+IMAGES_DIR = os.path.join(ROOT_DIR, IMAGES_PROJECT_PATH)
+ARTICLES_DIR = os.path.join(ROOT_DIR, ARTICLES_PROJECT_PATH)
+
 def get_latest_articles():
     latest_articles = []
     for a in _Article.query.order_by(_Article.dtime):
-        # Obtain the image and description paths from the name of the article.
-        image_root = 'static/images/{}'.format(
-            os.path.splitext(a.filepath.split('articles/')[-1])[0]
-        )
-        desc_path = a.filepath.replace('.html', '-desc.html')
-
-        # TODO:
-        # Get rid of the "TEST" modification once we have actual articles.
-        latest_articles.append({
-            'title': a.title.title().replace('Test', 'TEST'),
-            'filepath': a.filepath,
-            'image': image_root,
-            'desc': desc_path,
-        })
+        latest_articles.append({'name': a.name, 'category': a.category})
 
     return latest_articles
